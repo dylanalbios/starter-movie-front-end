edefdfed959b14af502e16e28b0cce3e6dda9ad0
dylanalbios <dylxanj430@gmail.com>
2023-08-13 18:48:32 -0700

    REACT_APP_API_URL converted back

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,5 @@
-require('dotenv').config();
-
 const API_BASE_URL =
-  process.env.PRODUCTION_API_URL || "http://localhost:5001";
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
